a5fbf6a80f35d0d6d4b705167ab787deb5bc3c8f
Jerry Breek <j.breek@tools4ever.com>
2022-10-18 16:24:20 +0200

    Updated to Graph API with audit-logging

--- Manual resources/[powershell-datasource]_Teams-get-team-channels.ps1
+++ Manual resources/[powershell-datasource]_Teams-get-team-channels.ps1
@@ -1,45 +1,46 @@
-#Input: TeamsAdminUser
-#Input: TeamsAdminPWD
-
-# Set TLS to accept TLS, TLS 1.1 and TLS 1.2
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
-
-$VerbosePreference = "SilentlyContinue"
-$InformationPreference = "Continue"
-$WarningPreference = "Continue"
-
-# variables configured in form
-$GroupId = $datasource.selectedTeam.groupId
-
-$connected = $false
+$groupId = $datasource.selectedValue.GroupId
+Write-Information $groupId
 try {
-	$module = Import-Module MicrosoftTeams
-	$pwd = ConvertTo-SecureString -string $TeamsAdminPWD -AsPlainText -Force
-	$cred = New-Object System.Management.Automation.PSCredential $TeamsAdminUser, $pwd
-	$teamsConnection = Connect-MicrosoftTeams -Credential $cred
-    Write-Information "Connected to Microsoft Teams"
-    $connected = $true
+    Write-Information -Message "Generating Microsoft Graph API Access Token user.."
+
+    $baseUri = "https://login.microsoftonline.com/"
+    $authUri = $baseUri + "$AADTenantID/oauth2/token"
+
+    $body = @{
+        grant_type      = "client_credentials"
+        client_id       = "$AADAppId"
+        client_secret   = "$AADAppSecret"
+        resource        = "https://graph.microsoft.com"
+    }
+
+    $Response = Invoke-RestMethod -Method POST -Uri $authUri -Body $body -ContentType 'application/x-www-form-urlencoded'
+    $accessToken = $Response.access_token;
+
+    #Add the authorization header to the request
+    $authorization = @{
+        Authorization = "Bearer $accesstoken";
+        'Content-Type' = "application/json";
+        Accept = "application/json";
+    }
+
+    $baseSearchUri = "https://graph.microsoft.com/"
+    
+    $channelUri = $baseSearchUri + "v1.0/teams" + "/$groupId/channels"
+    Write-Information $channelUri
+
+    $channels = Invoke-RestMethod -Uri $channelUri -Method Get -Headers $authorization -Verbose:$false
+    
+    foreach($channel in $channels.value)
+    {
+        $returnObject = @{Channel=$channel.DisplayName; Id=$channel.Id }
+        Write-Output $returnObject        
+    }
+    
 }
 catch
-{	
-    Write-Error "Could not connect to Microsoft Teams. Error: $($_.Exception.Message)"
-}
-
-if ($connected)
 {
-	try {
-		$teamChannels = Get-TeamChannel -GroupId $GroupId
-
-		if(@($teamChannels).Count -gt 0){
-		 foreach($teamChannel in $teamChannels)
-			{
-				$resultObject = @{DisplayName=$teamChannel.DisplayName; Description=$teamChannel.Description; Id=$teamChannel.Id;}
-				Write-Output $resultObject
-			}
-		}
-	}
-	catch
-	{
-		Write-Error "Error getting Team Channels. Error: $($_.Exception.Message)"
-	}
+    Write-Error "Error getting Team Details. Error: $($_.Exception.Message)"
+    Write-Warning -Message "Error getting Team Details"
+    return
 }
+
--- Manual resources/[task]_Teams - Remove Team Channel.ps1
+++ Manual resources/[task]_Teams - Remove Team Channel.ps1
@@ -1,39 +1,78 @@
-#Input: TeamsAdminUser
-#Input: TeamsAdminPWD
-
 # Set TLS to accept TLS, TLS 1.1 and TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
+
+$baseGraphUri = "https://graph.microsoft.com/"
 
 $VerbosePreference = "SilentlyContinue"
 $InformationPreference = "Continue"
 $WarningPreference = "Continue"
 
 # variables configured in form
-$groupId = $form.teams.GroupId
-$displayName = $form.Channels.DisplayName
+$groupid = $form.teams.GroupId
+$channelname = $form.channels.Channel
+$team = $form.teams.DisplayName
+$channelid = $form.channels.Id
 
-$connected = $false
-try {
-	$module = Import-Module MicrosoftTeams
-	$pwd = ConvertTo-SecureString -string $TeamsAdminPWD -AsPlainText -Force
-	$cred = New-Object System.Management.Automation.PSCredential $TeamsAdminUser, $pwd
-	$teamsConnection = Connect-MicrosoftTeams -Credential $cred
-    Write-Information "Connected to Microsoft Teams"
-    $connected = $true
-}
-catch
-{	
-    Write-Error "Could not connect to Microsoft Teams. Error: $($_.Exception.Message)"
-}
 
-if ($connected)
-{
-	try {
-		$removeChannel = Remove-TeamChannel -groupId $groupId -displayName $displayName
-		Write-Information "Successfully removed Channel [$displayName] from Team [$groupId]"
-	}
-	catch
-	{
-		Write-Error "Could not remove Channel [$displayName] from Team [$groupId]. Error: $($_.Exception.Message)"
+# Create authorization token and add to headers
+try{
+    Write-Information "Generating Microsoft Graph API Access Token"
+
+    $baseUri = "https://login.microsoftonline.com/"
+    $authUri = $baseUri + "$AADTenantID/oauth2/token"
+
+    $body = @{
+        grant_type    = "client_credentials"
+        client_id     = "$AADAppId"
+        client_secret = "$AADAppSecret"
+        resource      = "https://graph.microsoft.com"
+    }
+
+    $Response = Invoke-RestMethod -Method POST -Uri $authUri -Body $body -ContentType 'application/x-www-form-urlencoded'
+    $accessToken = $Response.access_token;
+
+    #Add the authorization header to the request
+    $authorization = @{
+        Authorization  = "Bearer $accesstoken";
+        'Content-Type' = "application/json";
+        Accept         = "application/json";
     }
 }
+catch{
+    throw "Could not generate Microsoft Graph API Access Token. Error: $($_.Exception.Message)"    
+}
+
+try {
+    Write-Information "Deleting Channel [$channelname] from team [$team]."
+
+    $deleteChannelUri = $baseGraphUri + "v1.0/teams/$groupid/channels/$channelid"
+
+    $deleteChannel = Invoke-RestMethod -Method DELETE -Uri $deleteChannelUri -Headers $authorization -Verbose:$false
+    
+    Write-Information "Successfully deleted channel [$channelname] from team [$team]."
+    $Log = @{
+        Action            = "DeleteResource" # optional. ENUM (undefined = default) 
+        System            = "MicrosoftTeams" # optional (free format text) 
+        Message           = "Successfully deleted channel [$channelname] from team [$team]." # required (free format text) 
+        IsError           = $false # optional. Elastic reporting purposes only. (default = $false. $true = Executed action returned an error) 
+        TargetDisplayName = $channelname # optional (free format text)
+        TargetIdentifier  = $channelid # optional (free format text)
+    }
+    #send result back  
+    Write-Information -Tags "Audit" -MessageData $log
+}
+catch
+{
+    Write-Error "Failed to delete channel [$channelname] from team [$team]. Error: $($_.Exception.Message)"
+    $Log = @{
+        Action            = "DeleteResource" # optional. ENUM (undefined = default) 
+        System            = "MicrosoftTeams" # optional (free format text) 
+        Message           = "Failed to delete channel [$channelname] from team [$team]." # required (free format text) 
+        IsError           = $true # optional. Elastic reporting purposes only. (default = $false. $true = Executed action returned an error) 
+        TargetDisplayName = $channelname # optional (free format text)
+        TargetIdentifier  = $channelid # optional (free format text)
+    }
+    #send result back  
+    Write-Information -Tags "Audit" -MessageData $log
+}
+
